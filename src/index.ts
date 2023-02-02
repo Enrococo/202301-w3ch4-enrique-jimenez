64b27344aa3afe24ee66a3ab455d13ff577a4017
`Enrique` <`e.jimenezmoreno@gmail.com`>
2023-02-02 18:48:59 +0100

    Add css styles and linked to heading

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import Header1 from './components/Heading/Heading.js';
 
-const link = new Header1('My Series', document.body, 'hey');
+const link = new Header1('My Series', document.body, 'main-header');
 link.render();
