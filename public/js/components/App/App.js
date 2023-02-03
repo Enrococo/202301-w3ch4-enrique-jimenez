6b7bb94bce62a22a0c54fec93f306b8ec1fc76f7
`Enrique` <`e.jimenezmoreno@gmail.com`>
2023-02-03 09:06:27 +0100

    Add app component

--- public/js/components/App/App.js
+++ public/js/components/App/App.js
@@ -1,10 +1,15 @@
+import Card from '../Card/Card.js';
 import Component from '../Component.js';
 import Header1 from '../Heading/Heading.js';
+import { series } from '../Card/Card.js';
 export default class App extends Component {
     #children;
-    constructor(parentElement) {
-        super(parentElement, 'div');
+    constructor(parentElement, cssClasses = '') {
+        super(parentElement, 'div', cssClasses);
         this.#children = [new Header1('My Series', this.element, 'main-header')];
+        for (let i = 0; i < series.length; i++) {
+            this.#children.push(new Card(this.element, i, 'series-list'));
+        }
     }
     render() {
         super.render();
--- src/components/App/App.ts
+++ src/components/App/App.ts
@@ -1,12 +1,17 @@
+import Card from '../Card/Card.js';
 import Component from '../Component.js';
 import Header1 from '../Heading/Heading.js';
+import { series } from '../Card/Card.js';
 
 export default class App extends Component {
   #children: Component[];
 
-  constructor(parentElement: HTMLElement | null) {
-    super(parentElement, 'div');
+  constructor(parentElement: HTMLElement | null, cssClasses: string = '') {
+    super(parentElement, 'div', cssClasses);
     this.#children = [new Header1('My Series', this.element, 'main-header')];
+    for (let i = 0; i < series.length; i++) {
+      this.#children.push(new Card(this.element, i, 'series-list'));
+    }
   }
 
   render(): void {
